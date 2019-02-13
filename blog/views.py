cdfb3e26513a992cbef28b1186d88f98c095cedd
writerlab <netspytux@gmail.com>
2019-02-13 10:01:21 +0700

    update

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return render(request, 'layout/index.html')
+  nama = "ZUL HILMI"
+  buah = ['Apel', 'Anggur', 'Pear', 'Jeruk']
+  return render(request, 'layout/index.html', {'nama':nama, 'buahbuahan':buah})
 
 def about(request):
   return render(request, 'layout/about.html')
 
 def kontak(request):
   return render(request, 'layout/kontak.html')
-
 
 def blog(request):
   return render(request, 'layout/blog.html')
