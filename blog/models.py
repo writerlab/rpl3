142510e922883f6cf920911b7cfb774e287818a8
writerlab <netspytux@gmail.com>
2019-02-20 10:08:43 +0700

    new model: Artikel

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class Artikel(models.Model):
+  judul = models.CharField(max_length=50)
+  tanggal = models.DateField()
+  penulis = models.CharField(max_length=20)
+  konten = models.TextField()
+  
+  def __unicode__(self):
+    return self.judul
+
