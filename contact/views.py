a38bfc788ad562926406491fea29557f62313c68
writerlab <netspytux@gmail.com>
2019-03-06 11:34:29 +0700

    new & modif

--- contact/views.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.shortcuts import render
-
-from .models import Contact
-
-
-def contact(request):
-  contacts = Contact.objects.all()
-  return render(request, 'layout/kontak.html', {'contacts':contacts})
--- rpl3/urls.py
+++ rpl3/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from blog.views import *
-from contact.views import contact
+from kontak.views import tampilkan_kontak
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
     url(r'^about/', about),
-    url(r'^kontak/', contact),
     url(r'^blog/', blog),
+    url(r'^kontak/', tampilkan_kontak),
 ]
